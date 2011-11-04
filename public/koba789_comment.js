293985257e10473046ca2398528be2486183d0cc
koba789 <kobahide789@gmail.com>
2011-11-05 00:27:21 +0900

    自動スクロールを実装

--- public/koba789_comment.js
+++ public/koba789_comment.js
@@ -7,14 +7,15 @@ var koba789 = {};
   var muteMessage = '###このコメントは表示されません###',
       destination = '' || null;
 
-  var commentList;
+  var commentList, commentContainer;
 
   /**
    * Initial Function
    */
   function init () {
     commentList = document.getElementById('koba789-comment-list');
-    
+    commentContainer = document.getElementById('koba789-comment-container');
+
     if (location.href.match(/^file:/)) return;
 
     var socket = io.connect(destnation);
@@ -25,7 +26,7 @@ var koba789 = {};
    * Test
    */
   function test () {
-    for (var i = 1; i <= 10; i ++) {
+    for (var i = 1; i <= 100; i ++) {
       onCommentReceived({text: 'テストメッセージ' + i.toString()});
     }
   }
@@ -36,7 +37,10 @@ var koba789 = {};
   function onCommentReceived (comment) {
     var text = comment.text;
     var isMuted = filter(text);
+
+    var isScrolled = isScrolledBottom();
     addComment(text, isMuted);
+    if (isScrolled) scrollToBottom();
   }
 
   /**
@@ -53,6 +57,17 @@ var koba789 = {};
     var newComment = document.createElement('li');
     newComment.textContent = newComment.innerText = isMuted ? muteMessage : text;
     commentList.appendChild(newComment);
+  }
+
+  function scrollToBottom () {
+    commentContainer.scrollTop = commentContainer.scrollHeight - commentContainer.clientHeight;
+  }
+
+  function isScrolledBottom () {
+    var scrollHeight = commentContainer.scrollHeight,
+	scrollTop = commentContainer.scrollTop,
+	clientHeight = commentContainer.clientHeight;
+    return scrollHeight <= clientHeight || scrollTop >= scrollHeight - clientHeight;
   }
 
   koba789.init = init;
